eb9ffad83b861636cc8269b7446552cf1321a1f1
Kevin Stahl <kevin@thestahlmarketing.com>
2026-08-05 11:13:05 -0500

    Initial Project structure using Vite Shopify Plugin

---
+++ frontend/components/App.jsx
@@ -0,0 +1,16 @@
+export default function App() {
+  return (
+    <div
+      style={{
+        padding: "1rem",
+        border: "2px solid hotpink",
+        borderRadius: "12px",
+        background: "#fff",
+      }}
+    >
+      <h2>🚀 Build a Box</h2>
+
+      <p>If you can see this, React is running.</p>
+    </div>
+  );
+}
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+import shopify from "vite-plugin-shopify";
+
 // Related: https://github.com/remix-run/remix/issues/2835#issuecomment-1144102176
 // Replace the HOST env var with SHOPIFY_APP_URL so that it doesn't break the Vite server.
 // The CLI will eventually stop passing in HOST,
@@ -45,10 +47,16 @@ export default defineConfig({
     hmr: hmrConfig,
     fs: {
       // See https://vitejs.dev/config/server-options.html#server-fs-allow for more information
-      allow: ["app", "node_modules"],
+      allow: ["app", "node_modules", "frontend"],
     },
   },
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [
+    reactRouter(),
+    tsconfigPaths(),
+    shopify({
+      themeRoot: "extensions/build-a-box",
+    }),
+  ],
   build: {
     assetsInlineLimit: 0,
   },
